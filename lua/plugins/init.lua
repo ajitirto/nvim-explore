c58cc14e3a1892f5fd14daabb251e7453352cdd5
ajitirto <ajiwoke@gmail.com>
2025-12-11 13:34:34 +0700

    plugin installed
    Signed-off-by: ajitirto <ajiwoke@gmail.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,21 +5,18 @@ return {
     require("plugins.user.nvim-treesitter"),
     require("plugins.user.json"),
     -- require("plugins.user.folke-snacks"),
-
     -- hardcode
     {
         "stevearc/conform.nvim",
         opts = require("configs.conform"),
         dependencies = { "mason.nvim" },
     },
-
     {
         "neovim/nvim-lspconfig",
         config = function()
             require("configs.lspconfig")
         end,
     },
-
     {
         "shellRaining/hlchunk.nvim",
         event = { "BufReadPre", "BufNewFile" },
@@ -29,141 +26,57 @@ return {
     },
     {
         "nvim-tree/nvim-tree.lua",
-        opts = {
-            view = {
-                side = "right",
-                width = 40,
-            },
-        },
+        opts = require("configs.nvim-tree"),
+        cmd = { "NvimTreeToggle", "NvimTreeFocus" },
     },
     {
         "mason-org/mason.nvim",
-        opts = {
-            ensure_installed = {
-                "ansible-language-server",
-                "ansible-lint",
-                "bash-language-server",
-                "black",
-                "blade-formatter",
-                "clangd",
-                "codespell",
-                "css-lsp",
-                "debugpy",
-                "delve",
-                "docker-compose-language-service",
-                "elixir-ls",
-                "erlang-debugger",
-                "erlang-ls",
-                "goimports",
-                "gopls",
-                "html-lsp",
-                "isort",
-                "json-lsp",
-                "kube-linter",
-                "laravel-ls",
-                "lemminx",
-                "lua-language-server",
-                "luau-lsp",
-                "omnisharp",
-                "php-cs-fixer",
-                "phpactor",
-                "phpcbf",
-                "pint",
-                "prettier",
-                "prettierd",
-                "rust-analyzer",
-                "sqlls",
-                "stylua",
-                "tofu-ls",
-                "typescript-language-server",
-                "yaml-language-server",
-            },
-
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗",
-                },
-            },
-            log_level = vim.log.levels.DEBUG,
-        },
+        opts = require("configs.mason"),
+        cmd = { "Mmason", "MasonInstall", "MasonUpdate" },
     },
     {
         "mfussenegger/nvim-dap",
         dependencies = {
-            "nvim-neotest/nvim-nio", -- Required by nvim-dap
-            "rcarriga/nvim-dap-ui", -- UI for nvim-dap
-            "theHamsta/nvim-dap-virtual-text", -- Virtual text for breakpoints
-            "nvim-telescope/telescope-dap.nvim", -- Telescope integration (optional)
+            "nvim-neotest/nvim-nio",
+            "rcarriga/nvim-dap-ui",
+            "theHamsta/nvim-dap-virtual-text",
+            "nvim-telescope/telescope-dap.nvim",
         },
         event = "VeryLazy",
         config = function()
-            require("dapui").setup({
-                -- Configure dapui as desired, e.g., layouts, icons
-                icons = { expanded = "▾", collapsed = "▸" },
-                layouts = {
-                    {
-                        elements = {
-                            { id = "scopes", size = 0.25 },
-                            "breakpoints",
-                            "stacks",
-                            "watches",
-                        },
-                        size = 10, -- columns
-                        position = "bottom",
-                    },
-                },
-            })
-
-            local dap = require("dap")
-            local dapui = require("dapui")
-
-            -- Automatically open/close dapui on attach/launch/terminate/exit
-            dap.listeners.before.attach.dapui_config = function()
-                dapui.open()
-            end
-            dap.listeners.before.launch.dapui_config = function()
-                dapui.open()
-            end
-            dap.listeners.before.event_terminated.dapui_config = function()
-                dapui.close()
-            end
-            dap.listeners.before.event_exited.dapui_config = function()
-                dapui.close()
-            end
+            require("configs.dap")
         end,
     },
-
-    -- Language-specific DAP integrations (example for Python)
     {
         "mfussenegger/nvim-dap-python",
-        ft = "python", -- Only load for Python files
+        ft = "python",
         dependencies = {
             "mfussenegger/nvim-dap",
         },
         config = function()
-            require("dap-python").setup({
-                -- Configure dap-python here, e.g., debugpy path
-                -- debugpy_path = "/path/to/your/debugpy",
-            })
+            require("dap-python").setup({})
         end,
     },
-
+    {
+        "mfussenegger/nvim-lint",
+        dependencies = { "mason.nvim" },
+        config = function()
+            require("configs.lint")
+        end,
+        event = "VeryLazy",
+    },
     { "nvzone/volt", lazy = true },
     { "nvzone/menu", lazy = true },
-
     {
         "nvzone/timerly",
         dependencies = "nvzone/volt",
         cmd = "TimerlyToggle",
-        opts = {}, -- optional
+        opts = require("configs.timerly"),
     },
-
     {
         "nvzone/floaterm",
         dependencies = "nvzone/volt",
-        opts = {},
+        opts = require("configs.floaterm"),
         cmd = "FloatermToggle",
     },
     {
@@ -175,21 +88,16 @@ return {
             require("configs.triforce")
         end,
     },
-
     {
         "wakatime/vim-wakatime",
         lazy = false,
     },
-
     -- markdown preview
     {
         "OXY2DEV/markview.nvim",
         lazy = false,
-        preview = {
-            icon_provider = "internal",
-        },
+        opts = require("configs.markview"),
     },
-
     -- todo nvim
     {
         "folke/todo-comments.nvim",
@@ -199,11 +107,10 @@ return {
             require("configs.todo")
         end,
     },
-
     -- trouble nvim
     {
         "folke/trouble.nvim",
-        opts = {},
+        opts = require("configs.trouble"),
         lazy = false,
         cmd = "Trouble",
     },
@@ -215,38 +122,9 @@ return {
             "saadparwaiz1/cmp_luasnip",
         },
         config = function()
-            local cmp = require("cmp")
-            cmp.setup({
-                sources = cmp.config.sources({ { name = "nvim_lsp" }, { name = "luasnip" } }),
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                }),
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").jump(args.body)
-                    end,
-                },
-            })
+            require("configs.cmp")
         end,
-    },
-    {
-        "mfussenegger/nvim-lint",
-        dependencies = { "mason.nvim" },
-        config = function()
-            require("lint").linters_by_ft = {
-                python = { "ruff" },
-                go = { "golangci-lint" },
-            }
-            vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-                callback = function()
-                    require("lint").try_lint()
-                end,
-            })
-        end,
+        event = "InsertEnter",
     },
     {
         "f-person/git-blame.nvim",
