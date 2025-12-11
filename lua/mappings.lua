6b0e60e968a4e8ff699ce3902f887255fae8d5f1
ajitirto <ajiwoke@gmail.com>
2025-12-11 13:35:08 +0700

    for key mapping
    Signed-off-by: ajitirto <ajiwoke@gmail.com>

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -12,6 +12,8 @@ map("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 map("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 map("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
 
+vim.keymap.set("n", "<C-p>", ":FloatermToggle<CR>", { silent = true })
+
 -- Nvim-tree keymaps
 map("n", "<C-b>", ":NvimTreeToggle<CR>", { desc = "Toggle NvimTree" })
 
@@ -25,19 +27,14 @@ end, {})
 -- mouse users + nvimtree users!
 vim.keymap.set({ "n", "v" }, "<RightMouse>", function()
     require("menu.utils").delete_old_menus()
-
     vim.cmd.exec('"normal! \\<RightMouse>"')
-
     -- clicked buf
     local buf = vim.api.nvim_win_get_buf(vim.fn.getmousepos().winid)
     local options = vim.bo[buf].ft == "NvimTree" and "nvimtree" or "default"
-
     require("menu").open(options, { mouse = true })
 end, {})
-
-vim.keymap.set("n", "<C-p>", ":FloatermToggle<CR>", { silent = true })
 
 vim.keymap.set("n", "<C-l>", function()
     require("triforce").show_profile()
 end, { desc = "tampilkan triforce statsnya" })
-vim.keymap.set("n", "<C-a>", ":Telescope<CR>", { noremap= true, silent = true })
+vim.keymap.set("n", "<C-a>", ":Telescope<CR>", { noremap = true, silent = true })
