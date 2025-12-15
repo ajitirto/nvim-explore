0ecf725d425efbe7c2030ee1aa5a92c9a30bbe29
ajitirto <ajiwoke@gmail.com>
2025-12-15 13:25:00 +0700

    zen mode width normal
    Signed-off-by: ajitirto <ajiwoke@gmail.com>

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -2,21 +2,15 @@ return {
     {
         "folke/zen-mode.nvim",
         opts = {
-            -- Configuration options go here
-            -- Example options:
             window = {
-                width = 0.85, -- 85% of the editor width
-                height = 1, -- Full height
-                backdrop = 0.95, -- backdrop transparency
+                width = 1,
+                height = 1,
+                backdrop = 0.95,
             },
-            -- Integrations with other plugins can be configured here
-            -- e.g., if you use folke/twilight.nvim to dim inactive code:
             plugins = {
                 twilight = { enabled = true },
             },
-            -- ... more options
         },
-        -- Optional: Define a keymap to easily toggle Zen Mode
         keys = {
             { "<leader>z", "<cmd>ZenMode<cr>", desc = "Toggle Zen Mode" },
         },
