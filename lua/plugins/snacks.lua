832cf061a8ab9b07761ba6cd16d2ace300b7f9f1
ajitirto <ajiwoke@gmail.com>
2025-12-16 17:40:14 +0700

    change notifier snacks 3 second

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -45,7 +45,7 @@ return {
                 force = false,
             },
             notifier = {
-                timeout = 8000,
+                timeout = 3000,
                 width = { min = 40, max = 0.4 },
                 height = { min = 1, max = 0.6 },
                 margin = { top = 0, right = 1, bottom = 0 },
