5a67de074a75cc2854acfef1c36348f082b7c5e3
ajitirto <ajiwoke@gmail.com>
2025-12-23 07:37:11 +0700

    custom multi cursor

--- lua/plugins/multicursor.lua
+++ lua/plugins/multicursor.lua
@@ -23,33 +23,17 @@ return {
         set({ "n", "x" }, "<A-S-down>", function()
             mc.lineSkipCursor(1)
         end, { desc = "Skip adding cursor below" })
-        set({ "n", "x" }, "<leader>mn", function()
-            mc.matchAddCursor(1)
-        end, { desc = "Add cursor to next match" })
-
-        set({ "n", "x" }, "<leader>ms", function()
-            mc.matchSkipCursor(1)
-        end, { desc = "Skip next match" })
-
-        set({ "n", "x" }, "<leader>mN", function()
-            mc.matchAddCursor(-1)
-        end, { desc = "Add cursor to previous match" })
-
-        set({ "n", "x" }, "<leader>mS", function()
-            mc.matchSkipCursor(-1)
-        end, { desc = "Skip previous match" })
 
         set("n", "<c-leftmouse>", mc.handleMouse, { desc = "Add cursor with mouse click" })
         set("n", "<c-leftdrag>", mc.handleMouseDrag, { desc = "Add cursors by mouse drag" })
-        set("n", "<c-leftrelease>", mc.handleMouseRelease, { desc = "Finish mouse cursor action" })
 
         set({ "n", "x" }, "<c-q>", mc.toggleCursor, { desc = "Toggle (enable/disable) current cursor" })
 
         mc.addKeymapLayer(function(layerSet)
-            layerSet({ "n", "x" }, "<left>", mc.prevCursor, { desc = "Focus previous cursor" })
-            layerSet({ "n", "x" }, "<right>", mc.nextCursor, { desc = "Focus next cursor" })
+            layerSet({ "n", "x" }, "<A-left>", mc.prevCursor, { desc = "Focus previous cursor" })
+            layerSet({ "n", "x" }, "<A-right>", mc.nextCursor, { desc = "Focus next cursor" })
 
-            layerSet({ "n", "x" }, "<leader>dx", mc.deleteCursor, { desc = "Delete current cursor" })
+            layerSet({ "n", "x" }, "<leader>dd", mc.deleteCursor, { desc = "Delete current cursor" })
 
             layerSet("n", "<esc>", function()
                 if not mc.cursorsEnabled() then
