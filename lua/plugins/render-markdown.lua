506b1d8d1cc9360c33d05d698aeb634faf67fbb9
ajitirto <ajiwoke@gmail.com>
2025-12-27 23:21:53 +0700

    render markdown langung jalan

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,6 +1,7 @@
 return {
     {
         "MeanderingProgrammer/render-markdown.nvim",
+        lazy = false,
         dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
         opts = {},
     },
