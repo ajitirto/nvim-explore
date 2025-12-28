e1454cab2e16ff403585e4e96b705f37eb8b6be7
ajitirto <ajiwoke@gmail.com>
2025-12-28 11:35:17 +0700

    timeout notifier 2000

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -45,7 +45,7 @@ return {
                 force = false,
             },
             notifier = {
-                timeout = 3000,
+                timeout = 2000,
                 width = { min = 40, max = 0.4 },
                 height = { min = 1, max = 0.6 },
                 margin = { top = 0, right = 1, bottom = 0 },
@@ -581,24 +581,6 @@ return {
                 end,
                 desc = "Prev Reference",
                 mode = { "n", "t" },
-            },
-            {
-                "<leader>N",
-                desc = "Neovim News",
-                function()
-                    Snacks.win({
-                        file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-                        width = 0.6,
-                        height = 0.6,
-                        wo = {
-                            spell = false,
-                            wrap = false,
-                            signcolumn = "yes",
-                            statuscolumn = " ",
-                            conceallevel = 3,
-                        },
-                    })
-                end,
             },
         },
     },
