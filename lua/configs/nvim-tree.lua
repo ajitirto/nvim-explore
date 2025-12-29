b89ff7c4a425f86ad09c2ac6386af1ad1008c876
ajitirto <ajiwoke@gmail.com>
2025-12-29 11:30:43 +0700

    comment width treesitter

--- lua/configs/nvim-tree.lua
+++ lua/configs/nvim-tree.lua
@@ -1,6 +1,6 @@
 return {
     view = {
         side = "right", -- Pohon file akan muncul di sisi kanan
-        width = 40, -- Lebar pohon file disetel ke 40 karakter
+        -- width = 40, -- Lebar pohon file disetel ke 40 karakter
     },
 }
