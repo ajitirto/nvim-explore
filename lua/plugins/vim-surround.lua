4016b2b90b0a5c71efd6e71c89e5d62761d9a6ab
ajitirto <ajiwoke@gmail.com>
2026-01-06 17:24:56 +0700

    add config in vim surround mapping

--- lua/plugins/vim-surround.lua
+++ lua/plugins/vim-surround.lua
@@ -3,5 +3,16 @@ return {
         "tpope/vim-surround",
         priority = 1000,
         lazy = false,
+        config = function()
+            -- Kita panggil which-key di sini untuk mendaftarkan labelnya
+            local status_ok, wk = pcall(require, "which-key")
+            if status_ok then
+                wk.add({
+                    { "ds", group = "Delete Surround" },
+                    { "cs", group = "Change Surround" },
+                    { "ys", group = "You Surround" },
+                })
+            end
+        end,
     },
 }
