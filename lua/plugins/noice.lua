c7cff886407604a9005a38a0d29029e3cb6dd1d0
ajitirto <ajiwoke@gmail.com>
2026-01-11 18:45:27 +0700

    fix completion lsp suggestion

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -9,14 +9,28 @@ return {
                 ["vim.lsp.util.stylize_markdown"] = true,
                 ["cmp.entry.get_documentation"] = true,
             },
+            hover = {
+                enabled = true,
+                silent = true,
+            },
+            signature = {
+                enabled = false,
+            },
         },
-
+        views = {
+            hover = {
+                border = {
+                    style = "rounded",
+                },
+                position = { row = 2, col = 2 },
+            },
+        },
         presets = {
             bottom_search = true,
             command_palette = true,
             long_message_to_split = true,
             inc_rename = false,
-            lsp_doc_border = false,
+            lsp_doc_border = true,
         },
     },
     dependencies = {
